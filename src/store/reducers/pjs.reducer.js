df2af82a60d49b48c153322c17def68e5197b93d
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-03-21 02:38:45 +0200

    version 1.2.1

--- src/store/reducers/pjs.reducer.js
+++ src/store/reducers/pjs.reducer.js
@@ -15,7 +15,7 @@ const initialState = {
 
 export const reduce = (state = initialState, action = {}) => {
 
-    console.log(action);
+    // console.log(action);
 
     switch (action.type) {
         
