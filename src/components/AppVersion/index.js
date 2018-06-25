ec05ed881f58e2d33925749480100036f0485d79
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-06-25 18:30:53 +0300

    added link to the repo from app version

--- src/components/AppVersion/index.js
+++ src/components/AppVersion/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './AppVersion.scss';
 
 import pack from '../../../package.json';
-
+const repo = `https://github.com/pandoraboxchain/pyrrha-webclient/tree/v${pack.version}`;
 
 const AppVersion = () => (
     <div className="pn-app-version">
-        v{pack.version}      
+        <a href={repo}>v{pack.version}</a>      
     </div>
 );
 
